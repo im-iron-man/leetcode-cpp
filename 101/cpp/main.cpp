fd0029648bfd1fcf59786579353ee49d3e94a779
gaoxinge <gaoxx5@gmail.com>
2021-06-25 14:29:31 +0800

    Create main.cpp

---
+++ 101/cpp/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* left, TreeNode *right) {
+        if (left == nullptr && right == nullptr) {
+            return true;
+        }
+        if ((left == nullptr && right != nullptr) || (left != nullptr && right == nullptr) || left->val != right->val) {
+            return false;
+        }
+        return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        return isSymmetric(root->left, root->right);
+    }
+};
