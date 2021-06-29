56c9112656ca95f03323e75201ba6d8d9783afc0
gaoxinge <gaoxx5@gmail.com>
2021-06-29 20:09:48 +0800

    Create main1.cpp

---
+++ 107/cpp/main1.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if (root == nullptr) {
+            return {};
+        }
+        
+        queue<pair<TreeNode*, int>> q;
+        q.push(make_pair(root, 0));
+        vector<vector<int>> r;
+        while (!q.empty()) {
+            pair<TreeNode*, int> p = q.front();
+            q.pop();
+            TreeNode* a = p.first;
+            int b = p.second;
+            if (b >= r.size()) {
+                r.insert(r.begin(), vector<int>{});
+            }
+            r[0].push_back(a->val);
+            if (a->left != nullptr) {
+                q.push(make_pair(a->left, b + 1));
+            }
+            if (a->right != nullptr) {
+                q.push(make_pair(a->right, b + 1));
+            }
+        }
+        
+        return r;
+    }
+};
