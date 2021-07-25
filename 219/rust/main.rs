55df63fab0233efe695468bcc50b7029c0f72b74
gaoxinge <gaoxx5@gmail.com>
2021-07-25 11:30:52 +0800

    Create main.rs

---
+++ 219/rust/main.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        use std::collections::HashMap;
+        let mut m = HashMap::new();
+        for i in 0..nums.len() {
+            let n = nums[i];
+            if m.contains_key(&n) && (i - m[&n]) as i32 <= k {
+                return true;
+            } else {
+                m.insert(n, i);
+                
+            }
+        }
+        return false;
+    }
+}
